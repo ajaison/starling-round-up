ce456ed72368624a5382df53d56582f0e7ddd3b4
alanjaison <ajaison61@googlemail.com>
2024-03-18 14:36:16 +0000

    Basic Spring app with dependencies and maven build from spring initializer.

---
+++ src/main/java/com/example/starling/StarlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.starling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/starling/StarlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
